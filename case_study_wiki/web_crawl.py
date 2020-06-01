62a4e182ee03af4764c2b7de59776a99c5d38d43
mohammad hossein zhoolideh haghighi <m.jolideh@gmail.com>
2020-06-01 09:43:32 +0430

    feat: while loop for itterating the craw function added

--- case_study_wiki/web_crawl.py
+++ case_study_wiki/web_crawl.py
@@ -20,3 +20,13 @@ def continue_crawl(search_history, target_url, max_steps=25):
     else:
         True
 
+import time
+
+while continue_crawl(article_chain, target_url): 
+    # download html of last article in article_chain
+    # find the first link in that html
+    first_link = find_first_link(article_chain[-1])
+    # add the first link to article chain
+    article_chain.append(first_link)
+    # delay for about two seconds
+    time.sleep(2)
